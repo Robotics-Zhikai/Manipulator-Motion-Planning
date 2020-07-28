11830919e3d4d0cc8be73bd8cad3f2a14a152a98
LAPTOP\zhikaiwang <979256120@qq.com>
2020-07-28 11:04:13 +0800

    readme

---
+++ MATLABCode/ParametersConfigCommon.m
@@ -0,0 +1,24 @@
+% ParametersConfigCommon
+global m0 m1 m2 m3 a0 a1 a2 a3 d1 d2 d3 d4 tool 
+global theta1Range theta2Range theta3Range theta4Range ZeroDefine
+
+m0=0*pi/180;
+m1=90*pi/180;
+m2=0*pi/180;
+m3=0*pi/180;
+a0=0;
+d3=0;
+d4=0;
+a1=12;
+a2=460;
+a3=210.9;
+d1=57.9;
+d2=13.7;
+tool=123.5;
+
+theta1Range = [-179.9999 180];
+theta2Range = [-40 44];
+theta3Range = [-130 -20];
+theta4Range = [-100 30];
+
+ZeroDefine = 10^-6;
